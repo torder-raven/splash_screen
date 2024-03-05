f70aa871057e82f0eda118c167a46580353c4e27
kite-torder <jjy5@torder.com>
2024-03-05 10:13:21 +0900

    fix: 로고 배치 방법 수정

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,15 +6,14 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'asset/img/splash/logo.png'
-          ),
-        ],
-      ),
-    );
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'asset/img/splash/logo.png',
+            ),
+          ],
+        ));
   }
 }
