a4f81f2d2ea79d42520e507cd525686dfb27c2ba
kite-torder <jjy5@torder.com>
2024-03-05 09:26:38 +0900

    feat: 홈 화면 로고 추가

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'asset/img/splash/logo.png'
+          ),
+        ],
+      ),
+    );
+  }
+}
