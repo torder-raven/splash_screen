eaa64f2d64266127de8d6daee665882b16730a6b
kite-torder <jjy5@torder.com>
2024-03-05 10:13:37 +0900

    feat: 텍스트 및 폰트 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -13,6 +13,14 @@ class HomeScreen extends StatelessWidget {
             Image.asset(
               'asset/img/splash/logo.png',
             ),
+            Text(
+              '테이블오더는 역시 티오더',
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.red,
+                  fontFamily: 'Pretendard',
+                  fontWeight: FontWeight.bold),
+            )
           ],
         ));
   }
